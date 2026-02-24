6e79ecb646931b1f8dd72a63ab9fba9d590d87f2
kingAndres19 <andresnicsegovia@hotmail.com>
2026-02-23 23:39:01 -0600

    añadido del tab

--- consulta-app/app/_layout.tsx
+++ consulta-app/app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from "expo-router";
+import {Tabs} from "expo-router"
+import { Ionicons } from '@expo/vector-icons';
 
-export default function RootLayout() {
-  return <Stack />;
-}
+export default function TabLayout() {
+  return(
+  <Tabs
+    screenOptions={{tabBarActiveTintColor: '#6395ec'}}
+  >
+    <Tabs.Screen
+      name="Inicio"
+      options={{
+        tabBarLabel:"Home",
+        tabBarIcon: ({color, size}) => <Ionicons name="home" color={color} size={size}/>
+      }}
+      
+    />
+
+  </Tabs>
+);}
--- consulta-app/app/index.tsx
+++ consulta-app/app/index.tsx
@@ -1,18 +1,58 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import React from 'react';
+import { ImageBackground, StyleSheet, Text, View, Image} from "react-native";
+import { Link, Stack } from "expo-router";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Para ir a la ventana</Text>
-      <Text>o a la ventana</Text>
-      <Link href="/acerca">Acerca</Link>
-    </View>
-  );
+    <>
+      <Stack.Screen options={{headerShown: false}}/>
+      <ImageBackground
+        source={require("../assets/images/17373.jpg")}
+        style={styles.background}
+        resizeMode='cover'
+        >
+          <View style = {styles.container} >
+            <Image
+              source={require("./../assets/images/Logo.png")}
+              style={styles.header} 
+            ></Image>
+
+            <Text style={styles.texto}>Para ir a la ventana</Text>
+            <Text>o a la ventana</Text>
+            <Link href="/acerca">Acerca</Link>
+          </View>
+    </ImageBackground>
+
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 24
+  },
+  header:{
+    marginVertical: 36,
+    width: 200,
+    height: 200,
+    borderWidth: 1,
+    borderRadius: 15,
+  },
+  texto: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  link: {
+    marginTop: 20,
+    color: '#007AFF', // Color azul estándar para enlaces
+    fontWeight: 'bold',
+  },
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+
+  },
+});
