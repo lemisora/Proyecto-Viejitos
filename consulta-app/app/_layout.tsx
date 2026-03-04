612141d90eb10b226c6f6108c21bd3aa525574af
kingAndres19 <andresnicsegovia@hotmail.com>
2026-03-03 22:02:27 -0600

    agregar Android

--- consulta-app/app/_layout.tsx
+++ consulta-app/app/_layout.tsx
@@ -1,14 +1,20 @@
 import {Tabs} from "expo-router"
 import { Ionicons } from '@expo/vector-icons';
+import TabTransparente from '@/components/TabBarTransparente';
 
 export default function TabLayout() {
   return(
   <Tabs
-    screenOptions={{tabBarActiveTintColor: '#6395ec'}}
+    screenOptions={{
+      tabBarActiveTintColor: '#6395ec',
+      tabBarInactiveTintColor: ' ',
+      tabBarBackground: TabTransparente,
+    }}
   >
     <Tabs.Screen
       name="Inicio"
       options={{
+
         tabBarLabel:"Home",
         tabBarIcon: ({color, size}) => <Ionicons name="home" color={color} size={size}/>
       }}
